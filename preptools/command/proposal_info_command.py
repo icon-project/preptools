503dfc07b03a17ea0219c08cdeecd4d9c6301bd0
Chiwon Cho <superdoli@gmail.com>
2022-04-06 18:11:21 +0900

    Update getProposals command
    * Add --start and --size options

--- preptools/command/proposal_info_command.py
+++ preptools/command/proposal_info_command.py
@@ -15,6 +15,8 @@
 # limitations under the License.
 
 from preptools.core.prep import create_reader_by_args
+from preptools.exception import InvalidArgumentException
+from preptools.utils import str_to_int
 
 
 def init(sub_parser, common_parent_parser):
@@ -24,7 +26,7 @@ def init(sub_parser, common_parent_parser):
 
 def _init_for_get_proposal(sub_parser, common_parent_parser):
     name = "getProposal"
-    desc = f"Inquire Proposal information using transaction hash"
+    desc = f"Query a proposal information with transaction hash"
 
     parser = sub_parser.add_parser(
         name,
@@ -50,7 +52,7 @@ def _get_proposal(args) -> dict:
 
 def _init_for_get_proposals(sub_parser, common_parent_parser):
     name = "getProposals"
-    desc = f"Inquire all of network proposal list."
+    desc = f"Query multiple network proposals."
 
     parser = sub_parser.add_parser(
         name,
@@ -59,18 +61,36 @@ def _init_for_get_proposals(sub_parser, common_parent_parser):
 
     parser.add_argument(
         "--type",
-        type=str,
+        type=str_to_int,
         required=False,
         nargs="?",
-        help="type of network proposal to filter"
+        help="Type of network proposal to filter"
     )
 
     parser.add_argument(
         "--status",
-        type=str,
+        type=str_to_int,
+        choices=range(6),
         required=False,
         nargs="?",
-        help="status of network proposal to filter"
+        help=(
+            "Status of network proposal to filter: \n"
+            "0(all), 1(applied), 2(disapproved), 3(canceled), 4(approved), 5(expired)"
+        ),
+    )
+
+    parser.add_argument(
+        "--start",
+        type=str_to_int,
+        nargs="?",
+        help="Starting index. 0 means the latest one",
+    )
+
+    parser.add_argument(
+        "--size",
+        type=str_to_int,
+        nargs="?",
+        help="Number of proposals to query. [1 ~ 10]",
     )
 
     parser.set_defaults(func=_get_proposals)
@@ -86,12 +106,20 @@ def _get_proposals(args) -> dict:
 
 
 def _check_get_proposal_list_args(args):
-    params = dict()
+    params = {}
 
     if hasattr(args, 'type') and args.type:
         params['type'] = args.type
 
     if hasattr(args, 'status') and args.status:
         params['status'] = args.status
+
+    if hasattr(args, 'start') and isinstance(args.start, int):
+        params['start'] = args.start
+
+    if hasattr(args, 'size') and isinstance(args.size, int):
+        if not (1 <= args.size <= 10):
+            raise InvalidArgumentException("size out of range: min(1), max(10)")
+        params['size'] = min(10, args.size)
 
     return params
